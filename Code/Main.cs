2e00b60e2ccff3877670fcc1ccbcac88ae03f108
LordVGames <lordvgames810@gmail.com>
2025-03-16 23:44:29 -0400

    Initial commit

---
+++ Code/Main.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using Mono.Cecil.Cil;
+using MonoMod.Cil;
+using RoR2;
+
+namespace GolemClapAttackSpeedFix
+{
+    internal static class Main
+    {
+        internal static Animator FixedGolemAnimator;
+
+        internal static void ClapState_OnEnter(ILContext il)
+        {
+            ILCursor c = new(il);
+
+            if (!c.TryGotoNext(MoveType.After,
+                x => x.MatchLdstr("Clap"),
+                x => x.MatchLdstr("Clap.playbackRate"),
+                x => x.MatchLdsfld<EntityStates.GolemMonster.ClapState>("duration")
+            ))
+            {
+                Log.Error("COULD NOT IL HOOK EntityStates.GolemMonster.ClapState.OnEnter");
+                LogILStuff(il, c);
+            }
+            else
+            {
+                MakeDurationUseAttackSpeed(c);
+            }
+        }
+
+        internal static void ClapState_FixedUpdate(ILContext il)
+        {
+            ILCursor c = new(il);
+
+            if (!c.TryGotoNext(MoveType.After,
+                x => x.MatchLdarg(0),
+                x => x.MatchCall(out _),
+                x => x.MatchLdsfld<EntityStates.GolemMonster.ClapState>("duration")
+            ))
+            {
+                Log.Error("COULD NOT IL HOOK EntityStates.GolemMonster.ClapState.FixedUpdate");
+                LogILStuff(il, c);
+            }
+            else
+            {
+                MakeDurationUseAttackSpeed(c);
+            }
+        }
+
+        internal static void ClapState_OnExit(ILContext il)
+        {
+            ILCursor c = new(il);
+
+            // don't need to go to any specific line, we can just throw our own il at the start and it's fine
+            c.Index = 0;
+            c.Emit(OpCodes.Ldarg_0);
+            c.EmitDelegate<Action<EntityStates.GolemMonster.ClapState>>((clapState) =>
+            {
+                clapState.modelAnimator.speed = 1;
+            });
+        }
+
+
+
+        private static void MakeDurationUseAttackSpeed(ILCursor c)
+        {
+            c.Emit(OpCodes.Ldarg_0);
+            c.EmitDelegate<Func<float, EntityStates.GolemMonster.ClapState, float>>((clapStateDuration, currentClapState) =>
+            {
+                currentClapState.modelAnimator.speed = 1 * currentClapState.attackSpeedStat;
+                return clapStateDuration / currentClapState.attackSpeedStat;
+            });
+        }
+
+        private static void LogILStuff(ILContext il, ILCursor c)
+        {
+            Log.Warning($"cursor is {c}");
+            Log.Warning($"il is {il}");
+        }
+    }
+}
---
+++ Code/Plugin.cs
@@ -0,0 +1,24 @@
+using BepInEx;
+using RoR2;
+
+namespace GolemClapAttackSpeedFix
+{
+    [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static PluginInfo PluginInfo { get; private set; }
+        public const string PluginGUID = PluginAuthor + "." + PluginName;
+        public const string PluginAuthor = "LordVGames";
+        public const string PluginName = "GolemClapAttackSpeedFix";
+        public const string PluginVersion = "1.0.0";
+        public void Awake()
+        {
+            PluginInfo = Info;
+            Log.Init(Logger);
+
+            IL.EntityStates.GolemMonster.ClapState.OnEnter += Main.ClapState_OnEnter;
+            IL.EntityStates.GolemMonster.ClapState.FixedUpdate += Main.ClapState_FixedUpdate;
+            IL.EntityStates.GolemMonster.ClapState.OnExit += Main.ClapState_OnExit;
+        }
+    }
+}
